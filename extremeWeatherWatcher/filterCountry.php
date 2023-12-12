0d0ee86ef263ac9bad0b04aec123ce42cec31aa1
LongNguyen <hoanglongst2000@gmail.com>
2023-12-12 15:41:23 -0800

    whoops

--- extremeWeatherWatcher/filterCountry.php
+++ extremeWeatherWatcher/filterCountry.php
@@ -1,7 +1,7 @@
 <?php
 require_once("assets/initializer.php");
 if(isset($_POST['filterCountryName']) ) {
-  if(isset($_POST['page'])){
+  if(isset($_POST['page']) && !empty($_POST['page'])){
     $page_num = $_POST['page'];
     $start_from = ($page_num - 1) * 10;
     showEventBasedOnCountries($_POST['filterCountryName'], 10, 10, $start_from);
